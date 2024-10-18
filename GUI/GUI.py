ca1b60a9cd317da2d9ad6c8967bca23607427993
Paper02 <Holacasa898@gmail.com>
2024-10-18 10:19:40 -0400

    adds some search bar functionality

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -1,11 +1,11 @@
 import sys
 import os
+
 from PySide6.QtCore import Qt
 
 from PySide6.QtGui import QColor, QPalette, QPixmap, QIcon
 
 from PySide6.QtWidgets import *
-from PySide6.QtWidgets import QWidget
 
 ICON_PATH: str = os.path.join(os.getcwd(), "GUI", "recipe_database_icon.png")
 PATH_TO_RECIPE: str = os.path.join(os.getcwd(), "mashed_potatoes.txt")
@@ -18,7 +18,6 @@ class Color(QWidget):
         palette = self.palette()
         palette.setColor(QPalette.Window, QColor(color))
         self.setPalette(palette)
-        
         
 
 #this class should be moved to another file that is not GUI.py
@@ -66,7 +65,6 @@ class MainWindow(QMainWindow):
         self.setMinimumWidth(600)
         
         self.setWindowIcon(QIcon(ICON_PATH))
-    
         self.start_ui()
         self.show()
 
@@ -83,12 +81,44 @@ class MainWindow(QMainWindow):
         
         self.search_bar = QLineEdit()
         self.search_bar.setPlaceholderText("Search for a recipe: (e.g. title:mashed potatoes ingredients:potatoes)")
-        
+        self.search_bar.textChanged.connect(self.on_search)
         #self.search_bar configuration
         self.search_bar.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
         
         self.scroll = QScrollArea()
+        
+        self.dock_widget = QDockWidget("Search Filters")
+        self.addDockWidget(Qt.RightDockWidgetArea, self.dock_widget)
+        
+        self.list_widget = QListWidget()
+        self.list_widget.addItems([
+            "Pepper", "Garlic", "Onion", "Tomatoes", 
+            "Chicken", "Beef", "Pork", "Fish", "Eggs", 
+            "Flour", "Rice", "Pasta", "Cheese", "Bread", "Carrots", 
+            "Potatoes", "Bell Peppers", "Broccoli", "Spinach", "Mushrooms", 
+            "Rosemary", "Oregano", 
+        ])
+        combo = QComboBox()
+        combo.addItems([
+            "All", "Italian", "Chinese", "Mexican", "Indian", 
+            "French", "Japanese", "Mediterranean", "Thai", 
+            "Spanish", "Greek", "Korean", "Vietnamese", 
+            "American", "Middle Eastern"
+        ])
+        for index in range(self.list_widget.count()):
+            item = self.list_widget.item(index)
+            item.setFlags(item.flags() | Qt.ItemIsUserCheckable)
+            item.setCheckState(Qt.Unchecked)
+        
 
+        self.dock_widget.setAllowedAreas(Qt.RightDockWidgetArea)
+        dock_layout = QVBoxLayout()
+        widget = QWidget()
+        widget.setLayout(dock_layout)
+        self.dock_widget.setWidget(widget)
+        dock_layout.addWidget(combo)
+        dock_layout.addWidget(self.list_widget)
+        self.dock_widget.setMaximumWidth(150)
         #self.scroll configuration
         self.scroll.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
         self.scroll.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
@@ -107,22 +137,31 @@ class MainWindow(QMainWindow):
         
         for i in range(6):
             self.sub_layout.addWidget(recipe_widget(Recipe(PATH_TO_RECIPE)).horizontal_item_widget())
+            
+    def restart_ui(self):
+        self.setCentralWidget(self.main_widget)
         
-        
-        
-    def add_row(self, row: QGroupBox):
-        self.layout.addWidget(row)
+    # this function should be overridden.
+    def search(self):
+        print(self.search_bar.text())
+
+    def on_search(self):
+        # todo: implement search functionality
+        self.search()
+        pass
+
         
 class recipe_widget:
-    def __init__(self, recipe: Recipe):
+    def __init__(self, recipe: Recipe, MainWindow: QMainWindow=None):
         
+        self.window = MainWindow
         self.recipe = recipe
         self.item_widget = None
         
     def get_widget(self):
         return self.widget
     
-    def horizontal_item_widget(self,):
+    def horizontal_item_widget(self):
 
         """
         Creates and returns a horizontal item widget for displaying a recipe.
@@ -155,9 +194,12 @@ class recipe_widget:
     
     
     def on_item_click(self, _event):
+        self.window: MainWindow = self.item_widget.window()
         
         self.recipe_view: QWidget = self.construct_recipe_view()
-        self.item_widget.window().setCentralWidget(self.recipe_view) 
+        self.window.dock_widget.setHidden(True)
+        self.window.centralWidget().setParent(None)
+        self.window.setCentralWidget(self.recipe_view)
 
     def construct_recipe_view(self):
         
@@ -168,7 +210,7 @@ class recipe_widget:
         instructions.setWordWrap(True)
         instructions.setAlignment(Qt.AlignTop)
         
-        push_button = QPushButton("Back")
+        push_button = QPushButton("<-- Back")
         push_button.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
         push_button.setBaseSize(100, 100)
         push_button.clicked.connect(self.on_back_click)
@@ -184,8 +226,8 @@ class recipe_widget:
         
         
     def on_back_click(self):
-        
-        self.recipe_view.window().start_ui()
+        self.window.dock_widget.setHidden(False)
+        self.window.restart_ui()
         
         
 
@@ -193,6 +235,9 @@ class recipe_widget:
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
+
+        
     window = MainWindow()
+    
 
     app.exec()
