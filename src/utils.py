23c008b570f829dcb7a8671631e9a263666b38ea
Enmanuel <holacasa898@gmail.com>
2024-10-24 12:55:21 -0400

    Refactor utils.py and add Recipe class documentation

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 #May help keep it for now.
+
 def get_recipe_info(self):
 
     recipe_parts = []
@@ -19,6 +20,19 @@ def get_recipe_info(self):
 
 
 class Recipe:
+    """
+    class to represent a recipe.
+    Attributes:
+    -----------
+    cui_type : str
+        The type of cuisine.
+    title : str
+        The title of the recipe.
+    ingredients : str
+        The ingredients required for the recipe.
+    instructions : str
+        The instructions to prepare the recipe.
+    """
     def __init__(self, recipe_as_list) -> None:
         self.cui_type = recipe_as_list[0]
         self.title = recipe_as_list[1]
