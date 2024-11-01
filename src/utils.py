8fbafb35b88c241884ae5f5b4b34f2f716983049
Paper-2 <65647380+Paper-2@users.noreply.github.com>
2024-11-01 12:10:39 -0400

    Update utils.py
    deletes an unused function.

--- src/utils.py
+++ src/utils.py
@@ -1,24 +1,3 @@
-#May help keep it for now.
-
-def get_recipe_info(self):
-
-    recipe_parts = []
-
-    while len(self.recipe) > 6:
-        index = self.recipe.find("---")
-
-        if index == -1:
-            recipe_parts.append(self.recipe)
-
-            break
-
-        recipe_parts.append(self.recipe[: index - 1])
-
-        self.recipe = self.recipe[index + 4 :]
-
-    return recipe_parts
-
-
 class Recipe:
     """
     class to represent a recipe.
